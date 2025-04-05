c68cb98ebd1289d1f84a38e089ca87d2303fb13f
L3gvccy <oleksandr_iv@knu.ua>
2025-04-05 15:42:51 +0300

    logo and favicon update

--- pcstore/pcstore/settings.py
+++ pcstore/pcstore/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
+STATICFILES_DISRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
