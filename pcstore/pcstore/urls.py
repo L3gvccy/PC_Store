67d780ecfe04c103d0b936f5f55d48bf47d7e3cb
L3gvccy <oleksandr_iv@knu.ua>
2025-04-06 22:54:47 +0300

    add header

--- pcstore/pcstore/urls.py
+++ pcstore/pcstore/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import components.views
 
 urlpatterns = [
+    path('', components.views.landing, name="landing"),
     path('admin/', admin.site.urls),
 ]
