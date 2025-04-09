64a115fc2297941ebf2d738b8c41483e1b013b6c
L3gvccy <oleksandr_iv@knu.ua>
2025-04-09 12:36:56 +0300

    update register

--- pcstore/account/views.py
+++ pcstore/account/views.py
@@ -1,18 +1,34 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 def register_user(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
 
-        user = User.objects.create_user(username=username, password=password, email=email,first_name = first_name,last_name = last_name)
+        if password != confirm_password:
+            messages.error(request, "Паролі не співпадають.")
+            return render(request, 'account/register.html')
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
         user.save()
 
-        return redirect('/account/register/') 
-    
+        messages.success(request, "Реєстрація успішна!")
+        return redirect('/account/register/')
+
     return render(request, 'account/register.html')
 
