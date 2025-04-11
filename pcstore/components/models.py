ef49329b346911886ea2dcabcf04c8a1785b8cee
L3gvccy <oleksandr_iv@knu.ua>
2025-04-11 17:35:44 +0300

    change cpu model

--- pcstore/components/models.py
+++ pcstore/components/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
 # Create your models here.
-
-class CPU(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    quantity = models.IntegerField()
+    image = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class CPU(Product):
     brand = models.CharField(max_length=25)
     socket = models.CharField(max_length=25)
     cores = models.IntegerField()
     threads = models.IntegerField()
     frequency = models.FloatField(help_text="GHz")
-    price = models.DecimalField(max_digits=20, decimal_places=2)
-    image = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
+    TDP = models.IntegerField(help_text="Watt", default=0)
