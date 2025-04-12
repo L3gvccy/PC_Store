0f12b3a2166e39b975c860d3a513adf1c6825f43
L3gvccy <oleksandr_iv@knu.ua>
2025-04-12 17:02:48 +0300

    add motherboards

--- pcstore/components/urls.py
+++ pcstore/components/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('cpus/', views.CPUs_view),
     path('cpu_detail/<int:cpu_id>/', views.cpu_detail, name='cpu_detail'),
+    path('motherboards/', views.Motherboards_view),
 ]
