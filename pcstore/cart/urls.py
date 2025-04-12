d39d9a1d94bb1d5a67a0d1f5f4a8f3432024b140
L3gvccy <oleksandr_iv@knu.ua>
2025-04-12 13:41:48 +0300

    add clear_cart

--- pcstore/cart/urls.py
+++ pcstore/cart/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('decrease/<int:item_id>/', views.decrease_quantity, name='decrease_quantity'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('clear_cart/', views.clear_cart, name='clear_cart'),
 ]
