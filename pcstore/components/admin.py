e1a014772c499aa3d0bd46d744dab6789b817571
Arsen Altunyan <arsenaltunyan@knu.ua>
2025-04-12 21:11:24 +0300

    Update CPU

--- pcstore/components/admin.py
+++ pcstore/components/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CPU, Motherboard
+from .models import CPU, Motherboard, GPU
 
 # Register your models here.
 admin.site.register(CPU)
 admin.site.register(Motherboard)
+admin.site.register(GPU)
