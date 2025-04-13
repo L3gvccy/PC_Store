218d31dd2ba2927a741937960e2d44d363d4e77d
L3gvccy <oleksandr_iv@knu.ua>
2025-04-13 22:41:12 +0300

    add components main page

--- pcstore/components/urls.py
+++ pcstore/components/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.components),
     path('cpus/', views.CPUs_view),
     path('cpu_detail/<int:cpu_id>/', views.cpu_detail, name='cpu_detail'),
     path('motherboards/', views.Motherboards_view),
