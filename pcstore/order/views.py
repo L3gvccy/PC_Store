0045f6f1f5e25a16df60cc661773522ab4cd0ac7
L3gvccy <oleksandr_iv@knu.ua>
2025-04-13 22:01:42 +0300

    add my_orders

--- pcstore/order/views.py
+++ pcstore/order/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 def checkout_view(request):
     user = request.user
     cart_items = CartItem.objects.filter(user=user)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
 
     if cart_items.count() == 0:
         messages.error(request, "Додайте хочаб один товар, щоб створити замволення.")
@@ -25,6 +26,7 @@ def checkout_view(request):
             order.last_name = request.POST['last_name']
             order.email = request.POST['email']
             order.phone_number = request.POST['phone_number']
+            order.total_price = total_price
             order.save()
 
             for item in cart_items:
@@ -42,10 +44,22 @@ def checkout_view(request):
     context = {
         'user': user,
         'cart_items': cart_items,
-        'form': form
+        'form': form,
+        'total_price': total_price,
     }
     return render(request, 'order/checkout.html', context)
 
 
 def order_success(request):
     return render(request, 'order/order_success.html')
+
+def view_orders(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Вам потрібно увійти в систему, щоб переглянути замовлення.")
+        return redirect(request.META.get('HTTP_REFERER', '/'))
+
+    orders = Order.objects.filter(user=request.user).order_by('-created_at')
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'order/my_orders.html', context)
