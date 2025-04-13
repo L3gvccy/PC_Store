675cfa44a3e5655bf012eb8f18ade71f4483b201
L3gvccy <oleksandr_iv@knu.ua>
2025-04-13 22:01:42 +0300

    add my_orders

--- pcstore/order/urls.py
+++ pcstore/order/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('checkout/', views.checkout_view, name='checkout'),
     path('order-success/', views.order_success, name='order_success'),
+    path('my_orders/', views.view_orders, name='my_orders'),
 ]
