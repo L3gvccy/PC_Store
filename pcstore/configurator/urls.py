4eadc74a3a6d1826571221b7e1fd3b5015af4e83
L3gvccy <oleksandr_iv@knu.ua>
2025-04-15 00:16:51 +0300

    add motherboard and RAM selection functionality to configurator

--- pcstore/configurator/urls.py
+++ pcstore/configurator/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.configurator_view, name="configurator"),
     path('select_cpu/<int:cpu_id>/', views.select_cpu, name="select_cpu"),
     path('remove_cpu/', views.remove_cpu, name="remove_cpu"),
+    path('select_mb/<int:mb_id>/', views.select_mb, name="select_mb"),
+    path('remove_mb/', views.remove_mb, name="remove_mb"),
+    path('select_ram/<int:ram_id>/', views.select_ram, name="select_ram"),
+    path('remove_ram/', views.remove_ram, name="remove_ram"),
 ]
