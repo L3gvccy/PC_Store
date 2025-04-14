82bb7ffe5655cb18a025b649f61ea38c325d4731
L3gvccy <oleksandr_iv@knu.ua>
2025-04-14 22:25:47 +0300

    add remove CPU functionality and update configurator template

--- pcstore/configurator/urls.py
+++ pcstore/configurator/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.configurator_view, name="configurator"),
     path('select_cpu/<int:cpu_id>/', views.select_cpu, name="select_cpu"),
+    path('remove_cpu/', views.remove_cpu, name="remove_cpu"),
 ]
