334896b9535b4db477479883dfbf2956dc82f1d4
L3gvccy <oleksandr_iv@knu.ua>
2025-04-16 19:09:54 +0300

    Update DEBUG setting to False and change page title in storage_detail.html

--- pcstore/pcstore/settings.py
+++ pcstore/pcstore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0y3(l3@#d60o(w8z)r$v+ba1by&$2f5)=!i^1&-rpo^rq(svb-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
