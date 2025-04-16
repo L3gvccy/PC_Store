1c1db5bb10f272ef7ba8a416b3f667e13759b866
Олександр Іванов <oleksandr_iv@knu.ua>
2025-04-16 19:56:58 +0300

    Delete pcstore directory

--- pcstore/order/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('checkout/', views.checkout_view, name='checkout'),
-    path('order-success/', views.order_success, name='order_success'),
-    path('my_orders/', views.view_orders, name='my_orders'),
-]
