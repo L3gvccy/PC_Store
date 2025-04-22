5470cde04a7ad20cb5ec55b1873f13bc3a0e9bf1
L3gvccy <oleksandr_iv@knu.ua>
2025-04-22 13:34:05 +0300

    Add CSRF trusted origins for secure requests

--- pcstore/pcstore/settings.py
+++ pcstore/pcstore/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://alive-veriee-l3gvccy-30167731.koyeb.app',
+]
